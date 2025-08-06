f057f87ee74864f02735382d727315bbe12dd1c9
HakanK003 <hkaratashk3@gmail.com>
2025-08-05 21:34:58 -0400

    First Commit

---
+++ habitapp/src/main/java/com/habitapp/habitapp/HabitappApplication.java
@@ -0,0 +1,13 @@
+package com.habitapp.habitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitappApplication.class, args);
+	}
+
+}
---
+++ habitapp/src/test/java/com/habitapp/habitapp/HabitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitapp.habitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
